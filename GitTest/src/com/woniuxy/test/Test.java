622a98d491b2d93144b197ba4e952c7e451caf70
wuxiurui <147971174@qq.com>
2019-06-17 15:46:36 +0800

    hebing

--- GitTest/src/com/woniuxy/test/Test.java
+++ GitTest/src/com/woniuxy/test/Test.java
@@ -1,5 +1,14 @@
 package com.woniuxy.test;
-
+/*
+ * 分支管理：
+ *     分支：git在管理项目的时候至少有一个分支master（主干）
+ *     		主干的作用：主要是提供公共的代码，一般的程序不要去对其进行修改
+ *     		一般情况下由项目经理、组长配置好环境之后推送到远程仓库，这些代码作为master
+ *     		其他的程序员、组员，应该在该分支以外的其他分支进行各自的开发
+ *     		宿舍管理模块：张三，应该新建一个分支branch-zhangsan，然后在该分支上进行代码编写，
+ *     					新建分支相当于是复制了一份master的代码
+ *     		学生管理模块：李四，也应该新建一个分支，在该分支上编写代码
+ */
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello git");
